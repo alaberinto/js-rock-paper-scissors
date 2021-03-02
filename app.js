4c7028544e11cea8b8ac2a4a906ac144f07f9f47
Alvin Laberinto <alvinlaberinto@rocketmail.com>
2021-03-01 21:35:21 -0700

    Added playerPlay function with case insensitivity.

--- app.js
+++ app.js
@@ -1,8 +1,51 @@
 function computerPlay() {
-    const computerOptions = ["Rock", "Paper", "Scissors"];
+    let computerOptions = ["Rock", "Paper", "Scissors"];
 
-    const random = Math.floor(Math.random() * computerOptions.length);
+    let random = Math.floor(Math.random() * computerOptions.length);
     return computerOptions[random];
 }
 
-console.log(computerPlay());
+function changeCase(playerSelection) {
+    return playerSelection.replace(
+        /\w\S*/g,
+        function (txt) {
+          return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
+      );
+}
+
+function playSingleRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        console.log("Draw!");
+    }
+    else {
+        if (playerSelection == "Rock") {
+            if (computerSelection == "Scissors") {
+                console.log("Rock beats scissors! Player wins!");
+            }
+            else if (computerSelection == "Paper") {
+                console.log("Paper beats rock! Computer wins!");
+            }
+        }
+        else if (playerSelection == "Paper") {
+            if (computerSelection == "Rock") {
+                console.log("Paper beats rock! Player wins!");
+            }
+            else if (computerSelection == "Scissors") {
+                console.log("Scissors beats paper! Computer wins!");
+            }
+        }
+        else if (playerSelection == "Scissors") {
+            if (computerSelection == "Paper") {
+                console.log("Scissors beats paper! Player wins!");
+            }
+            else if (computerSelection == "Rock") {
+                console.log("Rock beats scissors! Computer wins!");
+            }
+        }
+    }
+}
+
+let playerPlay = prompt("Choose: Rock, Paper, or Scissors.");
+playerPlay = changeCase(playerPlay);
+console.log(playerPlay);
